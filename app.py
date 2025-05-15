87096392e8a3515c4303d518674a7c75e92dd134
fazliddin40088 <fazliddin40088@gmail.com>
2025-05-15 08:09:09 +0500

    Fix port configuration

--- app.py
+++ app.py
@@ -182,4 +182,5 @@ def export_results():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
